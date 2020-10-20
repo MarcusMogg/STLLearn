bbbf98bfea678e15b0c77934acde9f70a3b2bdee
MarcusMogg <m1664907189@gmail.com>
2020-10-20 16:04:07 +0800

    add list

--- sequence_containers/list/README.md
+++ sequence_containers/list/README.md
@@ -1,5 +1,45 @@
-list是双向链表
+list是环状双向链表
+
+ps:这点在不同编译器里有不同的表现，stl源码剖析是这样说的,但是在vs里面会越界报错,比如
+```cpp
+    list<int> a = {1,2,3};
+
+    auto k = a.begin();
+    ++k;
+    ++k;
+    ++k;
+    ++k;
+    cout << *k;
+```
+在vs里会报错但是g++可以
+神必
 
 链表和vector 的区别就不用多说
 
 需要注意的是vector任何改变元素数量的操作都会导致所有迭代器失效（vector可能会copy到一个新的空间，地址改变了）, list不会
+
+核心操作是transfer操作
+
+```cpp
+    // 将[first,last)迁移到pos前面
+    void transfer(iterator pos, iterator first, iterator last)
+    {
+        if (pos != last)
+        {
+            last.node->prev->next = pos;
+            first.node->prev->next = last;
+            pos.node->prev->next = first;
+
+            iterator tmp = pos.node->prev;
+            pos.node->prev = last.node->prev;
+            last.node->prev = first.node->prev;
+            first.node->prev = tmp;
+        }
+    }
+```
+
+splice就是transfer
+
+merge(就是mergesort那个merge，不过吧数组改成链表)相当与一个链表的一部分不断transfer到另一个链表的相应位置
+
+sort和algorithm里的sort实现不一样，核心思想是mergesort 链表版
--- sequence_containers/list/my_list.h
+++ sequence_containers/list/my_list.h
@@ -1,6 +1,10 @@
+#include "../../allocator/my_alloc.h"
+#include "../../allocator/my_construct.h"
+
 #ifndef _MY_LIST_H_
 #define _MY_LIST_H_
-
+namespace MOG
+{
 template <class T>
 struct list_node
 {
@@ -20,7 +24,92 @@ struct list_iterator
     typedef T value_type;
     typedef Ptr pointer;
     typedef Ref reference;
-    typedef list_node<T> link_type;
+    typedef list_node<T> *link_type;
+    typedef size_t size_type;
+    typedef ptrdiff_t different_type;
+
+    link_type node;
+
+    list_iterator() {}
+    list_iterator(link_type x) : node(x) {}
+    list_iterator(const list_iterator &x) : node(x.node) {}
+
+    bool operator==(const self &x) const { return x.node == node; }
+    bool operator!=(const self &x) const { return x.node != node; }
+
+    // 这几个重载有点意思
+    reference operator*() const { return (*node).data; }
+    pointer operator->() const { return &(operator*()); }
+    // ++i
+    self &operator++()
+    {
+        node = node->next;
+        return *this;
+    }
+    // i++
+    self operator++(int)
+    {
+        self tmp = *this;
+        ++*this; //这里是调用了上面那个函数不是指针+
+        return tmp;
+    }
+    self &operator--()
+    {
+        node = node->prev;
+        return *this;
+    }
+    self operator--(int)
+    {
+        self tmp = *this;
+        --*this;
+        return tmp;
+    }
 };
 
+// list是一个环状双向链表
+template <class T, class Alloc = default_alloc>
+class my_list
+{
+public:
+    typedef list_node<T> node_type;
+    typedef node_type *link_type;
+    typedef list_iterator<T, T &, T *> iterator;
+    // 一个指针就可以表示环状双向链表
+    // node 指向尾端的一个空白指针
+    node_type node;
+
+    my_list(/* args */);
+    ~my_list();
+
+    iterator begin() { return node->next; }
+    iterator end() { return node; }
+    bool empty() const { return node->next == node; }
+
+    // 将x插入到pos前
+    iterator insert(iterator pos, const T &x)
+    {
+        link_type tmp; //= create_node(x); 分配一个node内存
+        tmp->next = pos.node;
+        tmp->prev = pos.node->prev;
+        pos.node->prev->next = tmp;
+        pos.node->prev = tmp;
+        return tmp;
+    }
+    // 将[first,last)迁移到pos前面
+    void transfer(iterator pos, iterator first, iterator last)
+    {
+        if (pos != last)
+        {
+            last.node->prev->next = pos;
+            first.node->prev->next = last;
+            pos.node->prev->next = first;
+
+            iterator tmp = pos.node->prev;
+            pos.node->prev = last.node->prev;
+            last.node->prev = first.node->prev;
+            first.node->prev = tmp;
+        }
+    }
+};
+} // namespace MOG
 #endif
